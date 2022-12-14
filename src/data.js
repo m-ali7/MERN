c63de95f78c60737321a3cb3bae5129bc183643c
Ali <2213005@cognizant.com>
2022-12-14 11:06:48 +0000

    first commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const data = {
+  products: [
+    {
+      name: 'White T-Shirt',
+      slug: 'white-t-shirt',
+      category: 'T-Shirts',
+      image: '/images/p1.jpg',
+      price: 50,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 10,
+      description: '100% cotton slim fit white t-shirt',
+    },
+
+    {
+        name: 'Black T-Shirt',
+        slug: 'black-t-shirt',
+        category: 'T-Shirts',
+        image: '/images/p2.jpg',
+        price: 50,
+        countInStock: 10,
+        rating: 4.8,
+        numReviews: 15,
+        description: '100% cotton slim fit black t-shirt',
+      },
+
+      {
+        name: 'Navy T-Shirt',
+        slug: 'navy-t-shirt',
+        category: 'T-Shirts',
+        image: '/images/p3.jpg',
+        price: 50,
+        countInStock: 10,
+        rating: 4.3,
+        numReviews: 7,
+        description: '100% cotton slim fit navy t-shirt',
+      },
+  ],
+};
